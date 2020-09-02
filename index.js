b96806c7fdc1bcb3d141826d8e3a8933744e3c00
obaidsiddiqui70 <70272102+obaidsiddiqui70@users.noreply.github.com>
2020-09-02 16:45:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var randomNumber1 = (Math.floor(Math.random()*6))+1;
 var randomNumber2 = (Math.floor(Math.random()*6))+1;
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-var folder1="images/"+randomDiceImage1;
-var folder2="images/"+randomDiceImage2;
+var folder1=randomDiceImage1;
+var folder2=randomDiceImage2;
 document.querySelectorAll('img')[0].setAttribute("src",folder1);
 document.querySelectorAll('img')[1].setAttribute("src",folder2);
 if(randomNumber1>randomNumber2)
